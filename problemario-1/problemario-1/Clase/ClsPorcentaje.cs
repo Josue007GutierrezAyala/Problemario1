c9582f742bea250d3b495696b636713c7c4e3a6d
gutie <gutie@LAPTOP-JM1T2PON>
2019-08-27 22:38:23 -0500

    3 er guardado

---
+++ problemario-1/problemario-1/Clase/ClsPorcentaje.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace problemario_1.Clase
+{
+    class ClsPorcentaje
+    {
+        public double Porcentaje(double N1, double N2)
+        {
+            double P;
+            P = N1 / 100 ;
+            return P;
+        }
+    }
+}
